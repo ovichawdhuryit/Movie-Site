9c855d335aadd6296da89225e98c9db9d0bc5e78
Anik Chawdhury <ovichawdhuryit@gmail.com>
2026-09-04 02:27:23 +0600

    added navbar

--- FrontEnd Movie Server/src/App.jsx
+++ FrontEnd Movie Server/src/App.jsx
@@ -1,20 +1,24 @@
 
 import './App.css'
-import MovieCard from "./Components/MovieCard" 
+import MovieCard from "./Components/MovieCard"
 import Home from './Pages/Home'
 import Favorites from './Pages/Favorites'
-import {Routes, Route} from "react-router-dom"
+import { Routes, Route } from "react-router-dom"
+import NavBar from './Components/NavBar'
 
 function App() {
 
 
   return (
-    <main calssName="main-content">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/favorites" element={<Favorites />} />
-       </Routes>
-    </main>
+    <div>
+      <NavBar />
+      <main calssName="main-content">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
+      </main>
+    </div>
   );
 }
 
